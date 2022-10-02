2492b05a51cdee78c2d74f5164aadb4d73629526
emmaglorypraise@gmail.com <emmaglorypraise@gmail.com>
2022-10-02 20:20:47 +0100

    add deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+//require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+import "@nomiclabs/hardhat-ethers";
+require("dotenv").config({ path: ".env" });
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+
+const ALCHEMY_GOERLI_API_KEY_URL = process.env.ALCHEMY_GOERLI_API_KEY_URL;
+//contract address key
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+//beneficial address key
+const ACCOUNT_PRIVATE_KEY2 = process.env.ACCOUNT_PRIVATE_KEY2;
+
+const INFURA_MAINNET_API_URL = process.env.INFURA_MAINNET_API_KEY_URL;
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      forking:{
+        url: INFURA_MAINNET_API_URL,
+      }
+    },
+    goerli: {
+      url: ALCHEMY_GOERLI_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: "API_TOKEN"
+  }
 };
-
-export default config;
